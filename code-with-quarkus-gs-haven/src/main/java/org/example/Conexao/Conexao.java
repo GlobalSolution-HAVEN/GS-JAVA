b3cc32e1965e20eba56b5159813ddb7f165c4631
Eduardo Augusto <edugamer0806@gmail.com>
2025-11-19 11:19:02 -0300

    Removi a classe conexao

--- code-with-quarkus-gs-haven/src/main/java/org/example/Conexao/Conexao.java
+++ code-with-quarkus-gs-haven/src/main/java/org/example/Conexao/Conexao.java
@@ -1,25 +1,25 @@
-package org.example.Conexao;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+// package org.example.Conexao;
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
 
-public class Conexao {
-    private static Connection conexao = null;
+// public class Conexao {
+//     private static Connection conexao = null;
 
-    private static final String server = "oracle.fiap.com.br";
-    private static final String port = "1521";
-    private static final String sid = "ORCL";
-    private static final String url = "jdbc:oracle:thin:@" + server + ":" + port + ":" + sid;
-    private static final String user = "RM565269";
-    private static final String password = "291206";
+//     private static final String server = "oracle.fiap.com.br";
+//     private static final String port = "1521";
+//     private static final String sid = "ORCL";
+//     private static final String url = "jdbc:oracle:thin:@" + server + ":" + port + ":" + sid;
+//     private static final String user = "RM565269";
+//     private static final String password = "291206";
 
-    public static Connection recuperaConexao() throws SQLException {
-        if (conexao == null) {
-            conexao = DriverManager.getConnection(url, user, password);
-        }
-        return conexao;
-    }
-}
+//     public static Connection recuperaConexao() throws SQLException {
+//         if (conexao == null) {
+//             conexao = DriverManager.getConnection(url, user, password);
+//         }
+//         return conexao;
+//     }
+// }
 
 
 
